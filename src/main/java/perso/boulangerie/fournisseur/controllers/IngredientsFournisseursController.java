16250cd64b661d66c707375f9ce85b7047056b17
Razafindrakoto Tsito Ny Avo Soa <153315298+TsitoNyAvoSoaRazaFindrakoto@users.noreply.github.com>
2025-01-21 03:03:37 +0300

    Update IngredientsFournisseursController.java

--- src/main/java/perso/boulangerie/fournisseur/controllers/IngredientsFournisseursController.java
+++ src/main/java/perso/boulangerie/fournisseur/controllers/IngredientsFournisseursController.java
@@ -10,7 +10,6 @@ import perso.boulangerie.produit.services.IngredientService;
 
 import java.util.List;
 
-
 @Controller
 @RequestMapping("/fournisseur/ingredients-fournisseurs")
 public class IngredientsFournisseursController {
@@ -28,13 +27,14 @@ public class IngredientsFournisseursController {
 
 	@GetMapping
 	public String getAllIngredientsFournisseurs(Model model) {
-		List<IngredientsFournisseurs> ingredientsFournisseursList = ingredientsFournisseursService.getIngredientsFournisseurss();
+		List<IngredientsFournisseurs> ingredientsFournisseursList = ingredientsFournisseursService
+				.getIngredientsFournisseurss();
 		model.addAttribute("ingredientsFournisseursList", ingredientsFournisseursList);
 		return "fournisseur/ingredients-fournisseurs/list";
 	}
 
 	@GetMapping("new/{fournisseur-id}")
-	public String showNewForm(@PathVariable("fournisseur-id") Integer fournisseurId,Model model) {
+	public String showNewForm(@PathVariable("fournisseur-id") Integer fournisseurId, Model model) {
 		model.addAttribute("ingredientsFournisseurs", new IngredientsFournisseurs());
 		model.addAttribute("fournisseur", fournisseurService.findFournisseur(fournisseurId));
 		model.addAttribute("ingredients", ingredientService.getAllIngredients());
@@ -44,12 +44,14 @@ public class IngredientsFournisseursController {
 	@PostMapping
 	public String saveIngredientsFournisseurs(@ModelAttribute IngredientsFournisseurs ingredientsFournisseurs) {
 		ingredientsFournisseursService.save(ingredientsFournisseurs);
-		return "redirect:/fournisseur/"+ingredientsFournisseurs.getFournisseur().getIdFournisseur();
+		return "redirect:/fournisseur/" + ingredientsFournisseurs.getFournisseur().getIdFournisseur();
 	}
 
 	@GetMapping("/edit/{id-ingredient}/{id-fournisseur}")
-	public String showEditForm(@PathVariable("id-ingredient") Integer idIngredient, @PathVariable("id-fournisseur") Integer idFournisseur, Model model) {
-		IngredientsFournisseurs ingredientsFournisseurs = ingredientsFournisseursService.findIngredientsFournisseurs(idIngredient, idFournisseur);
+	public String showEditForm(@PathVariable("id-ingredient") Integer idIngredient,
+			@PathVariable("id-fournisseur") Integer idFournisseur, Model model) {
+		IngredientsFournisseurs ingredientsFournisseurs = ingredientsFournisseursService
+				.findIngredientsFournisseurs(idIngredient, idFournisseur);
 		model.addAttribute("ingredientsFournisseurs", ingredientsFournisseurs);
 		return "fournisseur/ingredients-fournisseurs/form";
 	}
@@ -61,8 +63,10 @@ public class IngredientsFournisseursController {
 	}
 
 	@DeleteMapping("/delete/{id-ingredient}/{id-fournisseur}")
-	public String deleteIngredientsFournisseurs(@PathVariable("id-ingredient") Integer idIngredient, @PathVariable("id-fournisseur") Integer idFournisseur) {
-		IngredientsFournisseurs ingredientsFournisseurs = ingredientsFournisseursService.findIngredientsFournisseurs(idIngredient, idFournisseur);
+	public String deleteIngredientsFournisseurs(@PathVariable("id-ingredient") Integer idIngredient,
+			@PathVariable("id-fournisseur") Integer idFournisseur) {
+		IngredientsFournisseurs ingredientsFournisseurs = ingredientsFournisseursService
+				.findIngredientsFournisseurs(idIngredient, idFournisseur);
 		ingredientsFournisseursService.deleteIngredientsFournisseurs(ingredientsFournisseurs);
 		return "redirect:/fournisseur/ingredients-fournisseurs";
 	}
