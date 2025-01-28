63a358f0de421dbc33082eeee7a23104a1e542b6
LiokaTah <tahinalioka282@gmail.com>
2025-01-28 15:05:15 +0300

    change

---
+++ src/main/java/perso/boulangerie/produit/models/PrixProduit.java
@@ -0,0 +1,27 @@
+package perso.boulangerie.produit.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+@Entity
+public class PrixProduit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idPrixProduit;
+
+    @Column(nullable = false)
+    private LocalDate changement;
+
+    @Column(nullable = false, precision = 15, scale = 2)
+    private BigDecimal prixUnitaire;
+
+    @ManyToOne
+    @JoinColumn(name = "idProduit", nullable = false)
+    private Produit produit;
+}
+
